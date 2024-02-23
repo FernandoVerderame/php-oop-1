eeac619262a0c5b7ee22039f0bdbbe48c6fe3ab8
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-02-23 18:14:18 +0100

    printed movies list on page

--- index.php
+++ index.php
@@ -16,6 +16,16 @@ require __DIR__ . '/data/movies.php';
     <title>PHP Movie</title>
 </head>
 <body>
-    
+    <section id="movies">
+        <?php foreach ($movies as $movie) : ?>
+            <article>
+                <h2><?= $movie->title ?></h2>
+                <p><?= $movie->getAbstract() ?></p>
+                <div><?= $movie->language ?></div>
+                <div><?= $movie->vote ?></div>
+                <address><?= $movie->director->getFullName() ?></address>
+            </article>
+        <?php endforeach; ?>
+    </section>
 </body>
 </html>
