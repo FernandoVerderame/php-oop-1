5f12ee5ead6dc96b649bdc9f60f07393d9fa4147
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-02-23 17:46:36 +0100

    added movies array

--- data/movies.php
+++ data/movies.php
@@ -7,6 +7,6 @@ $movie_2 = new Movie(2, 'Saw X', 'John, malato e disperato, si reca in Messico p
 $movie_3 = new Movie(3, 'La vita è bella', 'Durante la dittatura fascista, Guido Orefice, giovane ebreo trasferitosi nella campagna toscana, conosce una maestra elementare, Dora, e con lei costruisce una famiglia. L\'aggravarsi delle Leggi Razziali e i rastrellamenti nazisti portano l\'uomo ad essere deportato in campo di concentramento con il figlioletto Giosuè. Per proteggere il piccolo dagli orrori dello sterminio, Guido costruisce eroicamente un elaborato mondo di vertiginose fantasie.', 'it', '5/5', $director_3);
 $movie_4 = new Movie(4, 'Perfetti sconosciuti', 'Una cena tra amici prende una strana piega quando accettano di lasciare i loro cellulari sul tavolo, rivelando il contenuto delle loro conversazioni e i segreti nascosti.', 'it', '4/5', $director_4);
 
-var_dump($movie_1, $movie_2, $movie_3, $movie_4);
+$movies = [$movie_1, $movie_2, $movie_3, $movie_4];
 
 ?>
