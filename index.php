cfdf4c3f430e334b277aa24e3c5d3c6629585771
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-02-23 15:54:51 +0100

    added movie class

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+// Movie class
+class Movie 
+{
+    public $id;
+    public $title;
+    public $overview;
+    public $language;
+    public $vote;
+};
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Movie</title>
+</head>
+<body>
+    
+</body>
+</html>
